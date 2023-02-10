4da91126b1035c74eca7b8a26bf0b48afaebac75
matheusprado1 <matheusprado1096@gmail.com>
2023-02-10 16:17:57 -0300

    feat: Update Views

--- backend/base/views.py
+++ backend/base/views.py
@@ -3,6 +3,8 @@ from django.http import JsonResponse
 from .products import products
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
 
 # Create your views here.
 routes = [
@@ -20,19 +22,21 @@ routes = [
     '/api/v1/products/<update>/<id>/',
 ]
 
+
 @api_view(http_method_names=['GET'])
 def getRoutes(request):
-  return Response(routes)
+    return Response(routes)
+
 
 @api_view(http_method_names=['GET'])
 def getProducts(request):
-  return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
 
 @api_view(http_method_names=['Get'])
 def getProduct(request, pk):
-  product = None
-  for i in products:
-    if i['_id'] == pk:
-      product = i
-      break
-  return Response(product)
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
