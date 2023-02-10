3093d8fb71604598a6693848097092441994eebc
matheusprado1 <matheusprado1096@gmail.com>
2023-02-10 13:09:21 -0300

    feat: Django cors

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -40,12 +40,15 @@ INSTALLED_APPS = [
 
     # 3rd-party apps
     'rest_framework',
+    'corsheaders',
 
     # my apps
     'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,3 +130,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
