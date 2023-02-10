91072e8f4252c1639dc94698e9745f1c4c38c121
matheusprado1 <matheusprado1096@gmail.com>
2023-02-10 12:19:28 -0300

    feat: Django API

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .products import products
 
 # Create your views here.
+routes = [
+    '/api/v1/products/',
+    '/api/v1/products/create',
+
+    '/api/v1/products/upload/',
+
+    '/api/v1/products/<id>/reviews/',
+
+    '/api/v1/products/top/',
+    '/api/v1/products/<id>/',
+
+    '/api/v1/products/delete/<id>/',
+    '/api/v1/products/<update>/<id>/',
+]
+
+def getRoutes(request):
+  return JsonResponse(routes, safe=False)
+
+def getProducts(request):
+  return JsonResponse(products, safe=False)
